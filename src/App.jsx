0a1449a637045389e21f92f60bb3336e566a4744
goetechs <114264011+goetechs@users.noreply.github.com>
2025-04-04 13:37:30 +0000

    Nettoyage des fichiers ignorés

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import React from "react";
 import "./index.css";
 import { About } from "./pages/about";
 import Testimonial from "./pages/Testimonial";
+import Projet from "./pages/projet";
 import Contact from "./pages/Contact";
 import { ToastContainer } from "react-toastify";
 import Footer from "./pages/Footer";
 import Header from "./pages/Header";
-import Projet from "./pages/projet";
 
 export default function App() {
   return (
