c3c82e6b21c4131f99094d62732faa7453129e9a
goetechs <114264011+goetechs@users.noreply.github.com>
2025-04-04 13:02:25 +0000

    simple fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import "./index.css";
 import { About } from "./pages/about";
-import Projet from "./pages/projet";
 import Testimonial from "./pages/Testimonial";
 import Contact from "./pages/Contact";
 import { ToastContainer } from "react-toastify";
 import Footer from "./pages/Footer";
 import Header from "./pages/Header";
+import Projet from "./pages/projet";
 
 export default function App() {
   return (
